9d60e36ebee92c481b117a9f9ac4586f9ddfaaef
Faizan Alam <faizansaikh786786f@gmail.com>
2026-02-28 17:10:30 +0530

    TapTalent Chat App

--- Backend/matchmaking/queue.js
+++ Backend/matchmaking/queue.js
@@ -1,14 +1,13 @@
-// matchmaking/queue.js
+
 const { v4: uuidv4 } = require("uuid");
 const chatRepo = require("../db/chatRepository");
 
 const waitingUsers = [];
 
 const addToQueue = (socket) => {
-  // already matched user ko queue me mat daalo
   if (socket.partner) return;
 
-  // duplicate prevent
+
   if (!waitingUsers.find(s => s.id === socket.id)) {
     waitingUsers.push(socket);
   }
@@ -20,7 +19,7 @@ const matchUsers = async () => {
   const user1 = waitingUsers.shift();
   const user2 = waitingUsers.shift();
 
-  // SAFETY CHECK
+
   if (!user1 || !user2) return;
 
   const sessionId = uuidv4();
@@ -33,7 +32,7 @@ const matchUsers = async () => {
 
   await chatRepo.createSession(sessionId, user1.id, user2.id);
 
-  // 🔥 IMPORTANT: emit to BOTH users explicitly
+
   user1.emit("matched", { partner: user2.id });
   user2.emit("matched", { partner: user1.id });
 };
